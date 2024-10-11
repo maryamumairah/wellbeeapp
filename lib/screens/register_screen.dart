8f3e8b5aba014782fcad797645380bd386d77b2b
Maryam Umairah <m.umairahyatim@live.iium.edu.my>
2024-10-11 10:36:58 +0800

    added custom_buttons.dart

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// import '../widgets/custom_buttons.dart';
+import '../widgets/custom_buttons.dart';
 import 'package:wellbeeapp/routes.dart';
 
 class RegisterScreen extends StatefulWidget {
